0461ff4a99c1e8e68718f17a5934f094340b836b
ksetherg <ksetherg@gmail.com>
2021-06-13 14:29:47 +0300

    Added model catboost

---
+++ core/Ml_model.py
@@ -0,0 +1,24 @@
+from catboost import CatBoostRegressor, CatBoostClassifier, Pool
+
+
+def set_model():
+    model_params = dict(
+            thread_count=8,
+            iterations=2000,
+            loss_function='CrossEntropy',
+            eval_metric='CrossEntropy',
+            allow_writing_files=False,
+            save_snapshot=False
+                        )
+    model = CatBoostClassifier()
+    model.set_params(**model_params)
+    return model
+
+
+def predict(test_df):
+    test_pool = Pool(data=test_df)
+    model = set_model()
+    model.load_model('model.cbm', format='cbm')
+    prediction = model.predict(test_pool, prediction_type='Probability')
+    prediction = pd.DataFrame(prediction, index=test_df.index)
+    return prediction
